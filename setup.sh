7ddf7183cf7cb4e864c113465eda9b9ab133d1c2
Ubuntu <ubuntu@ip-172-31-46-121.eu-west-2.compute.internal>
2021-03-31 16:10:11 +0000

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@
 
 	echo 'launching cluster'
 	eksctl create cluster \
-	--name DemoCluster \
+	--name JenkinsCluster \
 	--region eu-west-2 \
 	--nodegroup-name DemoNodes \
 	--nodes 2 \
 	--nodes-min 2 \
 	--nodes-max 3 \
-	--node-type t3.medium \
+	--node-type t2.micro \
 	--with-oidc \
 	--ssh-access \
 	--ssh-public-key TerraformPair \
