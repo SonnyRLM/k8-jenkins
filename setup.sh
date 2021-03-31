24cde896a31b05f27982d2c4a68ecf25cd44d9c1
Ubuntu <ubuntu@ip-172-31-46-121.eu-west-2.compute.internal>
2021-03-31 15:42:22 +0000

    added eks setup

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Script to deploy eks cluster 
+
+
+create_cluster() {
+	echo 'launching cluster'
+	eksctl create cluster \
+	--name DemoCluster \
+	--region eu-west-2 \
+	--nodegroup-name DemoNodes \
+	--nodes 2 \
+	--nodes-min 2 \
+	--nodes-max 3 \
+	--node-type t3.medium \
+	--with-oidc \
+	--ssh-access \
+	--ssh-public-key Terraform Pair \
+	--managed
+}
+
+check_cluster() {
+	eksctl get cluster -n DemoCluster
+}
