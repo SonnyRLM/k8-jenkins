b6fa77e17ef95dac84c60a8b66c72a96431690af
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-04-01 15:56:08 +0100

    Update reqInstall.sh

--- reqInstall.sh
+++ reqInstall.sh
@@ -7,7 +7,7 @@ sudo apt install -y unzip
 # Install and configure AWSCLI
 sudo apt install awscli -y
 
-aws configure set aws_access_key_id AKIAR6KCHRCQFY6CEF2T
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set default.region eu-west-2
 
