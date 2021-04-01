ce91a9314a925dbc8cec138cd0dfb0e55a6c3035
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-04-01 12:24:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 # Script to deploy eks cluster 
-clusterExist=$(eksctl get cluster | grep -w "JenkinsCluster5")
+clusterExist=$(eksctl get cluster | grep -w "JenkinsCluster")
 var_length=${#clusterExist}
 
 echo ${var_length}
 
 if (( ${var_length}==0 )); then
 echo 'launching cluster'
-eksctl create cluster --name JenkinsCluster5 --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t2.micro --with-oidc --ssh-access --ssh-public-key TerraformPair --managed
+eksctl create cluster --name JenkinsCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t2.micro --with-oidc --ssh-access --ssh-public-key TerraformPair --managed
 fi
