93ce6fe317dd6cbdd137eadb817be2ab2d5d91d2
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-04-01 12:24:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@
 clusterExist=$(eksctl get cluster | grep -w "JenkinsCluster")
 var_length=${#clusterExist}
 
-echo ${var_length}
-
 if (( ${var_length}==0 )); then
 echo 'launching cluster'
 eksctl create cluster --name JenkinsCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t2.micro --with-oidc --ssh-access --ssh-public-key TerraformPair --managed
