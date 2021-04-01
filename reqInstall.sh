473b460a718e6d40e385ab8f8c147ad5081f7924
SonnyRLM <jenkins@ip-172-31-34-6.eu-west-2.compute.internal>
2021-04-01 11:56:02 +0000

    aws install

--- reqInstall.sh
+++ reqInstall.sh
@@ -4,5 +4,5 @@ sudo apt install awscli
 
 echo ${AWS_SECRET_ACCESS_KEY}
 
-aws configure set aws_access_key_id
+aws configure set aws_access_key_id AKIAR6KCHRCQFY6CEF2T
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
