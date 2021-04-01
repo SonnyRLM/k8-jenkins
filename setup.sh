b5024776c30461c539c62358d9d776e52e7f4278
Sonny Milbourn <43782927+SonnyRLM@users.noreply.github.com>
2021-04-01 11:57:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,18 +8,5 @@ echo ${var_length}
 
 if [ 60==1 ] then
 echo 'launching cluster'
-eksctl create cluster \
---name JenkinsCluster \
---region eu-west-2 \
---nodegroup-name DemoNodes \
---nodes 2 \
---nodes-min 2 \
---nodes-max 3 \
---node-type t2.micro \
---with-oidc \
---ssh-access \
---ssh-public-key TerraformPair \
---managed
-else
-fi
+eksctl create cluster --name JenkinsCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t2.micro --with-oidc --ssh-access --ssh-public-key TerraformPair --managed
 fi
